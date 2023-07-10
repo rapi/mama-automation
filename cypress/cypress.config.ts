2aa7a44f19034c6d8b9ee7abd43a836d0c9a3f1f
Vasile Grinciuc <raptordias@gmail.com>
2023-07-10 09:31:51 +0300

    Add

--- cypress/cypress.config.ts
+++ cypress/cypress.config.ts
@@ -3,7 +3,9 @@ import {convert} from "./tasks/excel/convert";
 const data=require('dotenv').config()
 export default defineConfig({
   e2e: {
+    defaultCommandTimeout:11000,
     baseUrl: process.env.CYPRESS_HOST,
+    numTestsKeptInMemory: 0,
     chromeWebSecurity: false,
     specPattern: '**/*.cy.ts',
     setupNodeEvents(on, config) {
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -48,66 +48,89 @@ describe("spec.cy.js", () => {
             console.log(`Set name for ${name}`)
             cy.get('.td-date-client > #scrollable-dropdown-menu > .twitter-typeahead > .tt-input').type(name)
             const data = await promisify(cy.get('.td-date-client').find(".tt-suggestion"));
-            if (data.text().includes(judet)) {
-                for(const i in data){
-                    if(String(data[i].innerText).includes(judet)){
-                        data[i].click()
-                    }
+            let isNewClient = true
+
+            for (const i in data) {
+                if (String(data[i].innerText).includes(judet) && !/\d/.test(String(data[i].innerText))) {
+                    data[i].click()
+                    isNewClient = false
+                    break;
+
                 }
-                console.log(`Set existing client for ${name}`)
+            }
 
-            } else {
-                try{
-
+            if (isNewClient) {
                 // add new client
                 console.log(`Add new client for ${name}`)
-                data.click()
+                cy.get('.td-date-client > #scrollable-dropdown-menu > .twitter-typeahead > .tt-input').clear().type("new")
+                cy.get('.adauga-client-nou', {timeout: 100000}).should('exist');
+                cy.get('.adauga-client-nou', {timeout: 10000}).click()
+                cy.get('.modal-editare-client', {timeout: 100000}).should('exist');
+
+                //name
+                console.log(`Setup name for client ${name}`)
+                cy.get("#Client_Denumire").type(name)
+
                 //phone
                 console.log(`Setup phone for client ${name}`)
                 cy.get("#Client_Telefon1").type(phone)
+                console.log(`Setup address for client ${name}`)
+                cy.wait(1000)
+                cy.get('#Client_Adresa1').type("Strada: "+address.split('str.')?.[1]??street.replaceAll('str.',''))
+
+
                 cy.get(".judet>span").click()
                 cy.get(".select2-search__field").type(judet)
                 cy.get(".select2-results__option").click()
-                cy.get(".select2-search__field").type(cityM.replaceAll(' ', '-'))
+                // option
+                const option = await promisify(cy.get(".select2-search__field:first").type(cityM.replaceAll(' ', '-')))
+                if (option.length)
+                    cy.get(option).click()
+
                 cy.wait(1000)
                 cy.get(".select2-results__option:first").click()
 
                 //address
-                console.log(`Setup address for client ${name}`)
-                cy.wait(1000)
-                cy.get('#Client_Adresa1').type("Strada: "+street)
-                cy.get('.modal-editare-client',{timeout:100000}).should('not.exist');
-                // cy.get('.btn-salveaza-client').click()
-            }
-            catch(e){}
+                // cy.get('.close-modal-client:first').click()
+
+                cy.get('.modal-editare-client', {timeout: 100000}).should('not.exist');
+
             }
 
 
             //product
             console.log(`Setup product for client ${name}`)
-            const products=product
-            .split(" )")
+            const products = product
+                .split(" )")
             products.splice(-1)
-            for(const j in products){
+            for (const j in products) {
                 cy.wait(1500)
-                console.log(products[j])
-                const code=products[j].split(":")[0].trim()
-                console.log({code:products[j].split(" -  ")[0].split(' ( ')})
 
-                const price=products[j].split(" -  ")[0].split(' ( ')[1].trim()
-                console.log({code,price})
-
-                const quantity=products[j].split(" -  ")[1].trim()
-                console.log({code,price,quantity})
+                const code = products[j].split(":")[0].trim()
+                const price = products[j].split(" -  ")[0].split(' ( ')[1].trim()
+                const quantity = products[j].split(" -  ")[1].trim()
                 cy.get(".td-date-produs:last > #scrollable-dropdown-menu > .twitter-typeahead > .tt-input").type(code)
+                cy.get('.td-date-produs:last .tt-suggestion', {timeout: 100000}).should('exist');
 
+                const suggentions = await promisify(cy.get('.td-date-produs:last .tt-suggestion'));
+                console.log(code,suggentions)
 
-                const suggentions = await promisify(cy.get('.td-date-produs:last').find(".tt-suggestion:first"));
-                console.log(suggentions)
-                cy.get(".td-cantitate:last").type(quantity)
-                cy.get(".td-total:last").type(String(parseFloat(quantity)*parseFloat(price)))
+                for (const el of suggentions) {
+
+                    if (!String(el.innerText).includes(" ( ")) {
+                    console.log('select',el)
+                        cy.get(el).click()
+                        break;
+                    }
+
+                }
+                cy.wait(1000)
+
+                cy.get(".td-cantitate:last .text-box").type(quantity, {force: true})
+                cy.get(".td-total:last").type(String(parseFloat(quantity) * parseFloat(price)))
                 cy.get(".td-tva:last").type("19")
-                suggentions[0].click()
+                cy.get(".td-um:last").type("buc").type("{enter}")
+
                 cy.get(".btn-adauga-factura-continut").click()
             }
 
@@ -126,7 +149,11 @@ describe("spec.cy.js", () => {
             console.log(`Wait for save ${name}`)
             cy.scrollTo('top') // Scroll 'sidebar' to its bottom
             //
+            // cy.get('.btn-close-factura').click()
+            // cy.get('.btn-adauga-factura-inline').click()
+
             await promisify(cy.get('.td-date-produs:first > #scrollable-dropdown-menu > .twitter-typeahead > .tt-input', {timeout: 1000000}).should('have.value', ""))
+
             // console.log('gogo')
         }
 
